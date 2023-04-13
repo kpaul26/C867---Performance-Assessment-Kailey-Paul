0a4d669e486ce5a26509e0705bb208062b9e7e18
kpaul26 <98128008+kpaul26@users.noreply.github.com>
2023-04-13 03:53:21 -0700

    Add files via upload

--- C867 - Performance Assessment Kailey Paul/degree.h
+++ C867 - Performance Assessment Kailey Paul/degree.h
@@ -5,8 +5,8 @@
 using namespace std;
 
 //Enumerated data type DegreeProgram
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+enum DegreeProgram {SECURITY, NETWORK, SOFTWARE, NONE}; // Requirement C for enum DegreeProgram; sets a default of NONE for degree
 
-static const std::string DegreeProgramString[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+static const std::string DegreeProgramString[] = { "SECURITY", "NETWORK", "SOFTWARE", "NONE"}; 
 
 #endif
